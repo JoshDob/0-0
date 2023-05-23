17870f0bfd74600846a3142da077d048b9ee81af
Joshua Dobrowner <110553239+JoshDob@users.noreply.github.com>
2023-05-23 14:42:35 -0700

    starting

---
+++ src/components/Ring.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { currentSection } from "../store";
+    let section = 0;
+  
+    currentSection.subscribe((value) => {
+      section = value;
+    });
+  </script>
+  
+  <style>
+    .ring {
+      width: 500px;
+      height: 500px;
+      border: 2px solid #61dafb;
+      border-radius: 50%;
+      position: relative;
+    }
+  </style>
+  
+  <div class="ring"></div>
+  
---
+++ src/components/SelectionIcon.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { currentSection } from "../store";
+    export let section = 0;
+    let isActive = false;
+  
+    currentSection.subscribe((value) => {
+      isActive = section === value;
+    });
+  
+    function activateSection() {
+      currentSection.set(section);
+    }
+  </script>
+  
+  <style>
+    .section-icon {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #61dafb;
+      opacity: 0.5;
+      transition: opacity 0.3s ease;
+    }
+  
+    .section-icon.active {
+      opacity: 1;
+    }
+  </style>
+  
+  <div
+    class="section-icon {isActive ? 'active' : ''}"
+    on:click={activateSection}
+  ></div>
+  
