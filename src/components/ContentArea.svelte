066239963a3d9c94327f8224946fd3c13fd72099
Joshua Dobrowner <110553239+JoshDob@users.noreply.github.com>
2023-05-23 14:41:34 -0700

    1st

---
+++ src/components/ContentArea.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { currentSection } from "../store";
+    let section = 0;
+  
+    currentSection.subscribe((value) => {
+      section = value;
+    });
+  </script>
+  
+  <style>
+    .content-area {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #282c34;
+    }
+  </style>
+  
+  <div class="content-area">
+    <h1>Section {section}</h1>
+  </div>
